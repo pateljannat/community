36b9c486a9c375616ebee269dbb0cd7e427867f0
Jannat Patel <pateljannat2308@gmail.com>
2022-09-21 14:45:40 +0530

    fix: certificate through blob

--- lms/lms/doctype/lms_certificate/lms_certificate.py
+++ lms/lms/doctype/lms_certificate/lms_certificate.py
@@ -7,10 +7,19 @@ from frappe.utils import nowdate, add_years
 from frappe import _
 from frappe.utils.pdf import get_pdf
 from lms.lms.utils import is_certified
+from htmlwebshot import WebShot
+from lms.lms.utils import get_instructors
+from frappe.utils.jinja import render_template
+
 
 class LMSCertificate(Document):
 
-    def before_insert(self):
+
+    def validate(self):
+        self.validate_duplicate()
+
+
+    def validate_duplicate(self):
         certificates = frappe.get_all("LMS Certificate", {
             "member": self.member,
             "course": self.course
@@ -19,6 +28,7 @@ class LMSCertificate(Document):
             full_name = frappe.db.get_value("User", self.member, "full_name")
             course_name = frappe.db.get_value("LMS Course", self.course, "title")
             frappe.throw(_("{0} is already certified for the course {1}").format(full_name, course_name))
+
 
 @frappe.whitelist()
 def create_certificate(course):
@@ -43,8 +53,38 @@ def create_certificate(course):
         certificate.save(ignore_permissions=True)
         return certificate
 
+
 @frappe.whitelist()
 def get_certificate_pdf(html):
+    print(html)
     frappe.local.response.filename = "certificate.pdf"
     frappe.local.response.filecontent = get_pdf(html, {"orientation": "LandScape"})
     frappe.local.response.type = "pdf"
+
+
+@frappe.whitelist()
+def generate_image(name):
+
+    certificate = frappe.db.get_value("LMS Certificate", name,
+        ["name", "member", "issue_date", "expiry_date", "course"], as_dict=True)
+
+    course = frappe.db.get_value("LMS Course", certificate.course, ["title", "name", "image"], as_dict=True)
+    instructors = (", ").join([x.full_name for x in get_instructors(certificate.course)])
+    member = frappe.db.get_value("User", certificate.member, ["full_name"], as_dict=True)
+
+    logo = frappe.db.get_single_value("Website Settings", "banner_image")
+    template_name = frappe.db.get_single_value("LMS Settings", "custom_certificate_template")
+    custom_certificate_template = frappe.db.get_value("Web Template", template_name, "template")
+
+    template = custom_certificate_template if custom_certificate_template else "/templates/certificate.html"
+    html = render_template(template, {
+        "certificate": certificate,
+        "course": course,
+        "instructors": instructors,
+        "member": member,
+        "logo": logo
+    })
+
+    shot = WebShot()
+    shot.quality = 100
+    return shot.create_pic(html="<div>Jan</div>")
--- lms/www/courses/certificate.js
+++ lms/www/courses/certificate.js
@@ -1,53 +1,77 @@
 frappe.ready(() => {
 
+    get_canvas();
+
+    $("#export-as-png").click((e) => {
+        download_as_png();
+    });
+
     $("#export-as-pdf").click((e) => {
-        export_as_png(e);
-    })
+        download_as_pdf();
+    });
 
 });
 
-const export_as_pdf = (e) => {
 
-    var formData = new FormData();
+const load_certificate = () => {
+    frappe.call({
+        method: "lms.lms.doctype.lms_certificate.lms_certificate.generate_image",
+        args: {
+            "name": $("#certificate-parent").data("name")
+        },
+        callback: (data) => {
+            $("#certificate-parent").html(data.message)
+        }
+    });
+};
 
-    //Push the HTML content into an element
-    formData.append("html", $("#certificate-card").html());
 
+const get_canvas = () => {
+    html2canvas(document.querySelector('#certificate-card'), {
+        scrollY: -window.scrollY,
+        scrollX: 0
+    }).then((canvas) => {
+        canvas.toBlob((blob) => {
+            let url = URL.createObjectURL(blob);
+            $("#certificate-parent").html(`
+                <img width=100 height=100 id="certificate-image" src="${url}"></img>
+                <div>${url}</div>
+            `);
+            $("#certificate-html").addClass("hide");
+        });
+    })
+};
+
+
+const download_as_png = () => {
+    let a = document.createElement('a');
+    a.href = $("#certificate-image").attr("src");
+    a.download = $("#cta-parent").data("certificate-name") + ".pdf";
+    a.click();
+};
+
+
+const download_as_pdf = (canvas) => {
+    let xhr = new XMLHttpRequest();
+    let formData = new FormData();
+
+    /* formData.append("html", $("#certificate-parent").html());
     var blob = new Blob([], { type: "text/xml"});
-    formData.append("blob", blob);
-
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", '/api/method/lms.lms.doctype.lms_certificate.lms_certificate.get_certificate_pdf');
+    formData.append("blob", blob); */
+    formData.append("file_url", $("#certificate-image").attr("src"))
+    xhr.open("POST", '/api/method/frappe.handler.download_file');
     xhr.setRequestHeader("X-Frappe-CSRF-Token", frappe.csrf_token);
     xhr.responseType = "arraybuffer";
 
     xhr.onload = function(success) {
+        console.log(success)
         if (this.status === 200) {
             var blob = new Blob([success.currentTarget.response], {type: "application/pdf"});
             var objectUrl = URL.createObjectURL(blob);
-
             //Open report in a new window
             window.open(objectUrl);
         }
     };
     xhr.send(formData);
+};
 
-}
-
-const export_as_png = (e) => {
-    let button = $(e.currentTarget);
-    button.text(__("Exporting..."));
-
-    html2canvas(document.querySelector('#certificate-card'), {
-        scrollY: -window.scrollY,
-        scrollX: 0
-    }).then(function(canvas) {
-        let dataURL = canvas.toDataURL('image/png');
-        let a = document.createElement('a');
-        a.href = dataURL;
-        a.download = button.attr("data-certificate-name");
-        a.click();
-    }).finally(() => {
-        button.text(__("Export"))
-    });
-}
